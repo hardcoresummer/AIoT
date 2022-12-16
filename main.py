7e9c684c1681c43e7368864f0d78764c870f5185
hardcoresummer <ducthangnguyen0609@gmail.com>
2022-12-16 16:48:23 +0700

    send the image to adafruit feed

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import base64
 from AdafruitClient import AdafruitClient
 from MediatorInterface import Mediator
 from physical import ModbusMaster
-# from simple_ai import *
+
 import time
 
 from sensor_reader import MoistureReader, PlantHealthReader, TempReader
 
-
+import io
+import PIL.Image as Image
 
 class StateController(Mediator):
     def __init__(self,client:AdafruitClient,serial_connector:ModbusMaster,tempReader:TempReader,moistureReader:MoistureReader,plantHealthReader:PlantHealthReader) -> None:
@@ -56,7 +58,14 @@ class StateController(Mediator):
         self.ada_client.client.publish("sensor1",self.serial_connector.readMoisture()/100)
     def _handle_plant_health_event(self,event_data):
         label,image = event_data
-        self.ada_client.client.publish("plant health",label)
+        # convert image from numpy array to compressed stream
+        optim_stream = io.BytesIO()
+        Image.fromarray(image).save(optim_stream,format="JPEG",optimize=True,quality=60)
+        optim_stream.seek(0)
+
+        value = base64.b64encode(optim_stream.read())
+        self.ada_client.client.publish("plant-health",label)
+        self.ada_client.client.publish("plant-image",value)
 
         
 
